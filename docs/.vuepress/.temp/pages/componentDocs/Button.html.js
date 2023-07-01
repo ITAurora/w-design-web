4121c5f42a562c9cb96f7ee506715ebff1508883
Win <691871826@qq.com>
2023-07-01 16:12:53 +0800

    修改

--- docs/.vuepress/.temp/pages/componentDocs/Button.html.js
+++ docs/.vuepress/.temp/pages/componentDocs/Button.html.js
@@ -1,4 +1,4 @@
-export const data = JSON.parse("{\"key\":\"v-01b65d37\",\"path\":\"/componentDocs/Button.html\",\"title\":\"Input 输入框\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"基本使用\",\"slug\":\"基本使用\",\"link\":\"#基本使用\",\"children\":[]}],\"git\":{\"updatedTime\":1687533598000,\"contributors\":[{\"name\":\"Win\",\"email\":\"691871826@qq.com\",\"commits\":1}]},\"filePathRelative\":\"componentDocs/Button.md\"}")
+export const data = JSON.parse("{\"key\":\"v-01b65d37\",\"path\":\"/componentDocs/Button.html\",\"title\":\"Input 输入框\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"基本使用\",\"slug\":\"基本使用\",\"link\":\"#基本使用\",\"children\":[]}],\"git\":{\"updatedTime\":1688130921000,\"contributors\":[{\"name\":\"Win\",\"email\":\"691871826@qq.com\",\"commits\":2}]},\"filePathRelative\":\"componentDocs/Button.md\"}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
